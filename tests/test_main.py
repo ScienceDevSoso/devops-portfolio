24c878fb0d8f150588607c720b353768659900fa
Sohel <97291230+ScienceDevSoso@users.noreply.github.com>
2026-09-12 17:42:57 +0300

    Restore passing root endpoint test

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,7 +9,7 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
 
-    assert response.status_code == 500
+    assert response.status_code == 200
     assert response.json() == {"message": "DevOps Portfolio API"}
 
 def test_health():
