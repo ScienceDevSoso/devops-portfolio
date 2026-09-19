0b688139e33edefdb52237ea000327691baf9228
Sohel <97291230+ScienceDevSoso@users.noreply.github.com>
2026-09-19 17:51:22 +0300

    Add Terraform-managed security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,23 @@
 data "aws_vpc" "default" {
   default = true
 }
+
+resource "aws_security_group" "app" {
+  name        = "devops-portfolio-app"
+  description = "Security group for the DevOps portfolio application"
+  vpc_id      = data.aws_vpc.default.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.app.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_app_port" {
+  security_group_id = aws_security_group.app.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8000
+  to_port           = 8000
+  ip_protocol       = "tcp"
+}
