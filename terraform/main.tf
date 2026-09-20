cf371bd49f27057ba33dde6a9c9d04aca83ae107
Sohel <97291230+ScienceDevSoso@users.noreply.github.com>
2026-09-20 20:02:59 +0300

    Provision Kubernetes cluster with Amazon EKS

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,118 @@
 data "aws_vpc" "default" {
   default = true
 }
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "devops-portfolio-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "eks_node" {
+  name = "devops-portfolio-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ecr_pull_policy" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_eks_cluster" "app" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
+  version  = var.kubernetes_version
+
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
+  upgrade_policy {
+    support_type = "STANDARD"
+  }
+
+  vpc_config {
+    subnet_ids              = data.aws_subnets.default.ids
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    public_access_cidrs     = [var.developer_api_cidr]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+
+  tags = {
+    Name    = var.cluster_name
+    Project = "devops-portfolio"
+  }
+}
+
+resource "aws_eks_node_group" "app" {
+  cluster_name    = aws_eks_cluster.app.name
+  node_group_name = "devops-portfolio-nodes"
+  node_role_arn   = aws_iam_role.eks_node.arn
+  subnet_ids      = data.aws_subnets.default.ids
+
+  instance_types = [var.node_instance_type]
+  capacity_type  = "ON_DEMAND"
+  disk_size      = 20
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_ecr_pull_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy
+  ]
+
+  tags = {
+    Project = "devops-portfolio"
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,18 @@ output "default_vpc_cidr" {
   description = "CIDR block of the default VPC"
   value       = data.aws_vpc.default.cidr_block
 }
+
+output "eks_cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = aws_eks_cluster.app.name
+}
+
+output "eks_cluster_endpoint" {
+  description = "Kubernetes API endpoint"
+  value       = aws_eks_cluster.app.endpoint
+}
+
+output "eks_node_group_name" {
+  description = "Name of the EKS managed node group"
+  value       = aws_eks_node_group.app.node_group_name
+}
