759431a475b726874b5045a385a075b6a6a5e993
Sohel <97291230+ScienceDevSoso@users.noreply.github.com>
2026-09-20 18:52:32 +0300

    Retire standalone EC2 deployment

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,29 +3,3 @@ variable "aws_region" {
   type        = string
   default     = "eu-north-1"
 }
-
-variable "ami_id" {
-  description = "AMI ID used for the EC2 instance"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.micro"
-}
-
-variable "subnet_id" {
-  description = "Subnet where the EC2 instance is launched"
-  type        = string
-}
-
-variable "key_name" {
-  description = "Existing EC2 SSH key pair name"
-  type        = string
-}
-
-variable "developer_ssh_cidr" {
-  description = "CIDR allowed to SSH into the EC2 instance"
-  type        = string
-}
